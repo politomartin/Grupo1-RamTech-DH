f6e738ca1810c04b0d625b18972d4f26814d76e4
Martin <politomartin00@gmail.com>
2022-12-27 19:44:29 -0300

    Modificacion de ubicacion de carpetas

---
+++ ram-tech/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.get('/product-detail', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+})
+
+app.listen(3033, () => {
+    console.log('Servidor iniciado en http://localhost:3033');
+})
