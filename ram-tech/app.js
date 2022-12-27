1be550a0c681cb264b2eede9444eba97d3c6aecc
ConraA <aizawaconrado@gmail.com>
2022-12-27 20:13:54 -0300

    Archivo index

--- ram-tech/app.js
+++ ram-tech/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
