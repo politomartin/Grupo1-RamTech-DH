623120a30e9f7d2db44dc16e81967baa9ad3f80c
ConraA <aizawaconrado@gmail.com>
2023-01-11 20:30:08 -0300

    Separacion de vistas en carpetas

--- ram-tech/app.js
+++ ram-tech/app.js
@@ -9,19 +9,19 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
+    res.sendFile(path.join(__dirname, '/views/users/register.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'));
+    res.sendFile(path.join(__dirname, '/views/users/login.html'));
 })
 
 app.get('/product-detail', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+    res.sendFile(path.join(__dirname, '/views/products/productDetail.html'));
 })
 
 app.get('/product-cart', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productCart.html'));
+    res.sendFile(path.join(__dirname, '/views/products/productCart.html'));
 })
 app.listen(3033, () => {
     console.log('Servidor iniciado en http://localhost:3033');
